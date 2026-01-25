1200e275dbb98e6af9b265d595ab50c6e756fda3
niloy104 <manzurul104@gmail.com>
2026-01-26 00:28:39 +0600

    update main file for tokens

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,13 +3,20 @@ package main
 import (
 	"log"
 
+	"github.com/ianschenck/envflag"
 	"github.com/niloy104/Conduit/api/handler"
 	"github.com/niloy104/Conduit/api/server"
 	"github.com/niloy104/Conduit/api/storer"
 	"github.com/niloy104/Conduit/db"
 )
 
+const minSecretKeySize = 32
+
 func main() {
+	var secretKey = envflag.String("SECRET_kEY", "01234567890123456789012345678901", "secret key for jwt signing")
+	if len(*secretKey) < minSecretKeySize {
+		log.Fatalf("SECRET_KEY must me at leas %d charachter", minSecretKeySize)
+	}
 	db, err := db.NewDatabase()
 	if err != nil {
 		log.Fatal("error opening database: %w", err)
@@ -21,7 +28,7 @@ func main() {
 	// do something with the database
 	st := storer.NewMySQLStorer(db.GetDB())
 	srv := server.NewServer(st)
-	hdl := handler.NewHandler(srv)
+	hdl := handler.NewHandler(srv, *secretKey)
 	handler.RegisterRoutes(hdl)
 	handler.Start(":8080")
 }
